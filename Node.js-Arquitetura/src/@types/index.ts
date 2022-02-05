860f538f175fb4bdce8902c4f158ff52e2a96d64
Leandro Lopes <leandro_carlos@uol.com.br>
2022-02-05 10:26:01 -0300

    configuraçao express com typescript

--- Node.js-Arquitetura/src/@types/index.ts
+++ Node.js-Arquitetura/src/@types/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello World');
+import express, {Request, Response, NextFunction} from 'express';
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ status: 'ok' });
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
